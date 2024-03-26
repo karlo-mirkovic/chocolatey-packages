521b0fb30f3ad6389009a2a938b3e0a47f6e98a4
Karlo Mirkovic <karlo.mirkovic@pdfforge.org>
2024-03-26 11:17:03 +0100

    add terminal server package

---
+++ pdfcreator-terminal-server/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$checksumType  = 'sha256'
+$checksum      = 'E1783858C5E8408A27400157C77A4954776316D186B3C81FDE4CA736D3E4EBA4'
+$url           = 'https://download.pdfforge.org/download/pdfcreator-terminal-server/5.2.1/PDFCreator-Terminal-Server-5_2_1-Setup.exe?file=PDFCreator-Terminal-Server-5_2_1-Setup.exe&download'
+
+$packageArgs = @{
+  packageName   = 'pdfcreator-terminal-server'
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  url64bit      = $url
+  softwareName  = 'PDFCreator Terminal Server'
+  checksumType  = $checksumType 
+  checksum      = $checksum
+  checksum64    = $checksum
+  silentArgs    = "/COMPONENTS='MAINPROGRAM,HOTFOLDER,IMAGES2PDF' /VERYSILENT /DEFERLICENSECHECK"
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
