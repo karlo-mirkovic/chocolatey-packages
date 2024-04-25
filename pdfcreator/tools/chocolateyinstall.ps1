c26c85a3a7b803a7e4d5b156bd2bf1f8d67563df
Karlo Mirkovic <karlo.mirkovic@pdfforge.org>
2024-04-25 14:59:29 +0200

    add pdfcreator package

---
+++ pdfcreator/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$checksumType  = 'sha256'
+$checksum      = '9C68A989F89C233BB4E191E4A8C2F6AEF79D4B272A00B82A736E2368E0936F4D'
+$url           = 'https://download.pdfforge.org/download/pdfcreator/5.2.1/PDFCreator-5_2_1-Setup.exe?file=PDFCreator-5_2_1-Setup.exe&download'
+
+$packageArgs = @{
+  packageName   = 'pdfcreator'
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  url64bit      = $url
+  softwareName  = 'PDFCreator'
+  checksumType  = $checksumType
+  checksum      = $checksum
+  checksum64    = $checksum
+  silentArgs    = "/COMPONENTS='MAINPROGRAM' /SILENT"
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ pdfcreator/update.ps1
@@ -0,0 +1,26 @@
+Install-Module au
+
+$releases = 'https://download.pdfforge.org/download/pdfcreator/list'
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest $releases -UseBasicParsing
+    $regex = "\.exe$"
+    $latestVersionUrl = $download_page.Links | Where-Object href -match $regex | Select-Object -Expand href -First 1
+    $domain  = $releases -split '(?<=//.+)/' | Select-Object -First 1
+    $version = $latestVersionUrl -split '/' | Select-Object -Last 1 -Skip 1
+    @{
+        URL         = $domain + $latestVersionUrl + "&download"
+        Version     = $version
+    }
+}
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1"   = @{
+      "(?i)(^[$]url\s*=\s*)'.*'"                = "`${1}$($Latest.URL)`""
+      "(?i)(^[$]checksum\s*=\s*)'.*'"           = "`${1}$($Latest.Checksum)`""
+    }
+  }
+}
+
+update
